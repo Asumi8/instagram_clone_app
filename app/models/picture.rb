29fad6da384430281d76f3c514b0234d53114316
asumi nakajima <nakajima0848@gmail.com>
2022-10-11 16:35:58 +0900

    favorite:controllerの作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,4 +6,5 @@ class Picture < ApplicationRecord
   validates :content, presence: true, length: { maximum: 500 }
 
   has_many :favorites, dependent: :destroy 
+  has_many :favorite_users, through: :favorites, source: :user
 end
