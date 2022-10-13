2d0b37ccb24b07685cc2b1b89b4dc3707592a96e
asumi nakajima <nakajima0848@gmail.com>
2022-10-13 16:25:45 +0900

    seedデータの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password 
   validates :password, length: { minimum: 6 }
-  validates :image, presence: true
+  # validates :image, presence: true
   
   has_many :pictures
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
-# 50.times do |n|
-#   name = Faker::Internet.username
-#   email = Faker::Internet.email
-#   password = Faker::Internet.password
-#   image = "image.jpg"
-#   User.create!(name: name, 
-#               email: email, 
-#               password: password,
-#               image: image,
-#               )
-# end
+50.times do |n|
+  name = Faker::Internet.username
+  email = Faker::Internet.email
+  password = Faker::Internet.password
+  User.create!(name: name, 
+              email: email, 
+              password: password,
+              
+              )
+end
 
